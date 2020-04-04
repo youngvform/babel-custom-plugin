b692cf05d2bd8da3cc9408742aa07cd9dc70da4e
young-jinoh <austinhbk@naver.com>
2020-04-05 00:37:02 +0900

    initial commit

---
+++ plugins/insert-log.js
@@ -0,0 +1,26 @@
+const functionName = 'ad';
+
+module.exports = function ({ types: t }) {
+  return {
+    visitor: {
+      FunctionDeclaration(path) {
+        if (path.node.id.name.substr(0, 2) === functionName) {
+          path
+            .get('body')
+            .unshiftContainer(
+              'body',
+              t.expressionStatement(
+                t.callExpression(
+                  t.memberExpression(
+                    t.identifier('console'),
+                    t.identifier('log')
+                  ),
+                  [t.stringLiteral(`call ${path.node.id.name}`)]
+                )
+              )
+            );
+        }
+      },
+    },
+  };
+};
---
+++ src/code.js
@@ -0,0 +1,15 @@
+console.log('aaa');
+const v1 = 123;
+console.log('bbb');
+
+function onClick(e) {
+  const v = e.target.value;
+}
+
+function onChange(e) {
+  const v = e.target.value;
+}
+
+function add(a, b) {
+  return a + b;
+}
